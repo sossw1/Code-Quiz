1894964e89183d5d9f560fd57a76806ba596e6dd
sossw1 <sossw1@gmail.com>
2020-11-20 20:57:27 -0500

    Global vars

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+//Initialize variables
 var timeRemaining = 0;
+var introText = "Try your best to finish the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by 10 seconds!";
+var q1 = {
+    q: "question",
+    a: "incorrect",
+    b: "correct",
+    c: "incorrect",
+    d: "incorrect"
+}
+
+var q2 = {
+    q: "",
+    a: "",
+    b: "",
+    c: "",
+    d: ""
+}
+
+var q3 = {
+    q: "",
+    a: "",
+    b: "",
+    c: "",
+    d: ""
+}
+
 $("body").append("<nav>");
 $("nav").append("<a>View High Scores");
 $("nav").append(`<p>Time: ${timeRemaining}</p>`);
