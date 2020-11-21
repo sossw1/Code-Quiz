d482515f415d2f18bd9e52a05df6d3f6b0ff41d5
sossw1 <sossw1@gmail.com>
2020-11-20 21:57:42 -0500

    Created intro section

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //Initialize variables
 var timeRemaining = 0;
-var introText = "Try your best to finish the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by 10 seconds!";
 var q1 = {
     q: "question",
     a: "incorrect",
@@ -24,8 +23,14 @@ var q3 = {
     c: "",
     d: ""
 }
+var questions = [q1, q2, q3];
+var qNum = 0;
 
 $("body").append("<nav>");
 $("nav").append("<a href=''>View High Scores");
 $("nav").append(`<p>Time: ${timeRemaining}</p>`);
-$("body").append(`<section>${introText}`);
+$("body").append(`<section id="intro-section">`);
+var introSectionEl = $("#intro-section");
+introSectionEl.append("<h1>Code Quiz Challenge");
+introSectionEl.append("<p>Try your best to finish the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by 10 seconds!");
+
